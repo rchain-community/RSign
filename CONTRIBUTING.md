6298ceb32c1eaed1f44a8afc77db8684c7426de3
Dan Connolly <dckc@madmode.com>
2018-09-09 22:05:03 -0500

    dev tools: eslint, flow

---
+++ CONTRIBUTING.md
@@ -0,0 +1,8 @@
+## Code Style: Airbnb, mostly
+
+Run `npm run lint`. See `eslintrc.yaml` for deviations from Airbnb
+style.
+
+## Static type checking with flow
+
+Run `npm run check`.
--- rhotest.js
+++ rhotest.js
@@ -1,9 +1,11 @@
+/* global window */
+
 import { fromJSData, toByteArray, toRholang } from './RHOCore.js';
 
 window.fromJSData = fromJSData;
 
-export function run() {
-  const data = "Bob";
+export default function run() {
+  const data = 'Bob';
   const par = fromJSData(data);
   const bs = toByteArray(par);
   const rho = toRholang(par);
